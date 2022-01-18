b2c58d9edef484864c9a9ccc9309926c24825354
Chrisxavix <chrischillogallid64@outlook.com>
2022-01-17 22:44:36 -0500

    feat: dao with bd

---
+++ src/main/java/com/cursojava/curso/dao/UsuarioDao.java
@@ -0,0 +1,11 @@
+package com.cursojava.curso.dao;
+
+import com.cursojava.curso.models.Usuario;
+
+import java.util.List;
+
+/* Permite la comunicación con la base de datos */
+public interface UsuarioDao {
+
+    List<Usuario> getUsuarios();
+}
